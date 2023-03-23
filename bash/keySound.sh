45bc5c8376ae7d41e04b604961d4fae08facea73
jDevID <dav.botton@gmail.com>
2023-03-23 15:17:39 +0100

    now functional on every devices (pavucontrol pkg)

--- bash/keySound.sh
+++ bash/keySound.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 DIRECTION=$1
+
 if [ "$DIRECTION" == "+" ]; then
-  amixer set Master 5%+
+  pactl set-sink-volume @DEFAULT_SINK@ +5%
 elif [ "$DIRECTION" == "-" ]; then
-  amixer set Master 5%-
+  pactl set-sink-volume @DEFAULT_SINK@ -5%
 fi
+
+
+
+#   REQUIREMENTS  #
+
+# pavucontrol
+# initial wasn't modifying third party sound device like bluetooth
+# modifying on sound_card at control_name wasn't working at all
