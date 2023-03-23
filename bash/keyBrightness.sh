dbad787ed0a1e7ae553ed7d2d99e1995d0323aa9
jDevID <dav.botton@gmail.com>
2023-03-23 15:02:24 +0100

    update to handle all monitors

--- bash/keyBrightness.sh
+++ bash/keyBrightness.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-MONITOR=$(xrandr | grep -w connected | awk '{print $1}')
 DIRECTION=$1
 
-CURRENT_BRIGHTNESS=$(xrandr --verbose | grep -i -m 1 "$MONITOR" -A 7 | grep -i brightness | awk '{print $2}')
-if [ "$DIRECTION" == "+" ]; then
-  NEW_BRIGHTNESS=$(echo "$CURRENT_BRIGHTNESS + 0.15" | bc)
-elif [ "$DIRECTION" == "-" ]; then
-  NEW_BRIGHTNESS=$(echo "$CURRENT_BRIGHTNESS - 0.15" | bc)
-fi
-xrandr --output "$MONITOR" --brightness "$NEW_BRIGHTNESS"
+# Get all connected monitors
+MONITORS=$(xrandr | grep -w connected | awk '{print $1}')
+
+for MONITOR in $MONITORS; do
+  CURRENT_BRIGHTNESS=$(xrandr --verbose | grep -i -m 1 "$MONITOR" -A 7 | grep -i brightness | awk '{print $2}')
+
+  if [ "$DIRECTION" == "+" ]; then
+    NEW_BRIGHTNESS=$(echo "$CURRENT_BRIGHTNESS + 0.15" | bc)
+  elif [ "$DIRECTION" == "-" ]; then
+    NEW_BRIGHTNESS=$(echo "$CURRENT_BRIGHTNESS - 0.15" | bc)
+  fi
+
+  xrandr --output "$MONITOR" --brightness "$NEW_BRIGHTNESS"
+done
